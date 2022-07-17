6a295219521bd22ad8815c2a6a6e09d80ec04829
thalestmm <meier.thales@gmail.com>
2022-07-17 09:41:54 -0300

    Fixed disciplines filepath

--- Source/disciplines.py
+++ Source/disciplines.py
@@ -9,7 +9,7 @@ def get_all_discipline_names() -> List[str]:
     This will be used to select the adequate discipline on both the questions API and the Exam Reports.
     :return: List of strings containing all the registered disciplines.
     """
-    folder_path = r"./Dependencies/Disciplines"
+    folder_path = r"../Dependencies/Disciplines"
     full_filename = os.listdir(folder_path)
     output = []
 
@@ -26,7 +26,7 @@ def get_all_areas_from_discipline(discipline_name: str) -> List[str]:
     :param discipline_name: discipline name, as registered in the Disciplines Dependencies folder.
     :return: List containing all the Areas (without repetition)
     """
-    filepath = f"Dependencies/Disciplines/{discipline_name}.csv"
+    filepath = f"../Dependencies/Disciplines/{discipline_name}.csv"
     discipline_df = pd.read_csv(filepath, index_col="id")
 
     return list(discipline_df["area"].unique())
@@ -39,12 +39,13 @@ def get_all_subjects_from_area(discipline_name: str, area_name: str) -> List[str
     :param area_name: area name, as registered in the Disciplines Dependencies folder.
     :return: List containing all the Subjects
     """
-    filepath = f"Dependencies/Disciplines/{discipline_name}.csv"
+    filepath = f"../Dependencies/Disciplines/{discipline_name}.csv"
     discipline_df = pd.read_csv(filepath, index_col="id")
 
     return list(discipline_df.loc[discipline_df.area == area_name]["subject"].unique())
 
 
 if __name__ == "__main__":
+    print(get_all_discipline_names())
     print(get_all_areas_from_discipline("MATEMÁTICA"))
     print(get_all_subjects_from_area("MATEMÁTICA", "NÚMEROS COMPLEXOS"))
