6f175c53639702e22d7a5feecd6593e494ae55f1
thalestmm <meier.thales@gmail.com>
2022-07-17 11:59:49 -0300

    Finished setting up tests for disciplines.py

--- Source/disciplines.py
+++ Source/disciplines.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import List
 import pandas as pd
@@ -26,10 +27,16 @@ def get_all_areas_from_discipline(discipline_name: str) -> List[str]:
     :param discipline_name: discipline name, as registered in the Disciplines Dependencies folder.
     :return: List containing all the Areas (without repetition)
     """
-    filepath = f"../Dependencies/Disciplines/{discipline_name}.csv"
-    discipline_df = pd.read_csv(filepath, index_col="id")
+    discipline_name = discipline_name.upper()
 
-    return list(discipline_df["area"].unique())
+    filepath = f"../Dependencies/Disciplines/{discipline_name}.csv"
+    try:
+        discipline_df = pd.read_csv(filepath, index_col="id")
+        return list(discipline_df["area"].unique())
+
+    except FileNotFoundError:
+        logging.exception(f"'{discipline_name}' discipline not registered")
+        raise KeyError("discipline_name not available in Disciplines folder")
 
 
 def get_all_subjects_from_area(discipline_name: str, area_name: str) -> List[str]:
@@ -39,10 +46,24 @@ def get_all_subjects_from_area(discipline_name: str, area_name: str) -> List[str
     :param area_name: area name, as registered in the Disciplines Dependencies folder.
     :return: List containing all the Subjects
     """
-    filepath = f"../Dependencies/Disciplines/{discipline_name}.csv"
-    discipline_df = pd.read_csv(filepath, index_col="id")
+    discipline_name = discipline_name.upper()
+    area_name = area_name.upper()
 
-    return list(discipline_df.loc[discipline_df.area == area_name]["subject"].unique())
+    filepath = f"../Dependencies/Disciplines/{discipline_name}.csv"
+
+    try:
+        discipline_df = pd.read_csv(filepath, index_col="id")
+        subjects_from_area = list(discipline_df.loc[discipline_df.area == area_name]["subject"].unique())
+
+        if len(subjects_from_area) == 0:
+            logging.exception(f"{area_name} passed is not available inside the {discipline_name} file")
+            raise KeyError(f"area_name passed is not available inside the {discipline_name} file")
+
+        return subjects_from_area
+
+    except FileNotFoundError:
+        logging.exception(f"{discipline_name} discipline not registered")
+        raise KeyError("discipline_name not available in Disciplines folder")
 
 
 if __name__ == "__main__":
--- Tests/test_disciplines.py
+++ Tests/test_disciplines.py
@@ -1,9 +1,30 @@
 import unittest
+from Source import disciplines
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+# TODO: ADD ALL TESTS
+class TestDisciplines(unittest.TestCase):
+    def test_get_discipline_names(self):
+        self.assertEqual(disciplines.get_all_discipline_names()[0],"INGLÊS")
+
+    def test_get_all_areas(self):
+        _expected = ['COMPREENSÃO E INTERPRETAÇÃO DE TEXTOS', 'ESTRUTURAS GRAMATICAIS']
+        self.assertEqual(disciplines.get_all_areas_from_discipline("INGLÊS"), _expected)
+        self.assertEqual(disciplines.get_all_areas_from_discipline("inglês"), _expected)
+
+        with self.assertRaises(KeyError):
+            disciplines.get_all_areas_from_discipline("Testing")
+
+    def test_get_all_subjects(self):
+        _expected = ["Introdução à termologia", "Termometria", "Dilatação térmica de sólidos e líquidos"]
+        self.assertEqual(disciplines.get_all_subjects_from_area("FÍSICA","TERMOLOGIA"), _expected)
+        self.assertEqual(disciplines.get_all_subjects_from_area("FÍSICA","termologia"), _expected)
+
+        with self.assertRaises(KeyError):
+            disciplines.get_all_subjects_from_area("TESTING", "TESTING")
+
+        with self.assertRaises(KeyError):
+            disciplines.get_all_subjects_from_area("MATEMÁTICA", "TESTING")
 
 
 if __name__ == '__main__':
