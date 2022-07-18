c243d02b35284dff34b04725d6530ebdce6d8309
thalestmm <meier.thales@gmail.com>
2022-07-18 14:53:18 -0300

    Implemented tests for random_generation and validations for the same method

--- Source/interactions.py
+++ Source/interactions.py
@@ -1,16 +1,25 @@
 import logging
 import pandas as pd
 from random import randint
+from typing import Tuple
 
 
-# TODO: CHANGE DIFFICULTY PARAMETER TO A RANGE OF INTEGERS
-# TODO: ADJUST question_statistics.csv FILEPATH
 class Interactions:
     def __init__(self, parent_dir: str = "../Registry"):
         self.statistics_filepath = parent_dir + r"/question_statistics.csv"
 
     def get_random_question_from_parameters(self, discipline: str, area: str = None, subject: str = None,
-                                            difficulty_level: int = None):
+                                            difficulty_level: Tuple[int,int] = (1, 5)) -> str:
+        """
+        Generate a random question from the passed parameters.
+        The only parameter required is discipline, the others are optional.
+        If there isn't a question that fits the filters, a LookupError will be raised.
+        :param discipline:
+        :param area:
+        :param subject:
+        :param difficulty_level: tuple of integers representing the min level and the max level : (min, max)
+        :return: string representation of the question_id
+        """
         with open(self.statistics_filepath, "r") as file:
             questions_df = pd.read_csv(file, sep=";")
 
@@ -19,12 +28,22 @@ class Interactions:
             raise TypeError("Discipline must be passed as a string")
 
         filtered_df = questions_df.loc[questions_df.discipline == discipline]
+
         if area is not None:
             filtered_df = filtered_df.loc[filtered_df.area == area]
+
         if subject is not None:
             filtered_df = filtered_df.loc[filtered_df.subject == subject]
+
         if difficulty_level is not None:
-            filtered_df = filtered_df.loc[filtered_df.difficulty_level == difficulty_level]
+            if type(difficulty_level) is not tuple:
+                logging.exception(f"difficulty_level passed as type {type(difficulty_level)}")
+                raise TypeError("difficulty_level must be a tuple of integers")
+
+            if difficulty_level[0] is not None:
+                filtered_df = filtered_df.loc[filtered_df.difficulty_level >= difficulty_level[0]]
+            if difficulty_level[1] is not None:
+                filtered_df = filtered_df.loc[filtered_df.difficulty_level <= difficulty_level[1]]
 
         available_indexes = filtered_df.shape[0] - 1
 
@@ -32,12 +51,12 @@ class Interactions:
             logging.exception(f"No questions available for {discipline}/{area}/{subject}")
             raise LookupError("There are no questions that satisfy the search criteria")
 
-        random_question = filtered_df.iloc[randint(0, available_indexes)].question_id
+        random_question_id = filtered_df.iloc[randint(0, available_indexes)].question_id
 
-        return random_question
+        return random_question_id
 
 
 if __name__ == "__main__":
     interactions = Interactions(parent_dir="../Registry")
-    random_question = interactions.get_random_question_from_parameters(discipline="FÍSICA")
-    print(random_question)
+    random_question = interactions.get_random_question_from_parameters(discipline="TESTING")
+    print(type(random_question))
--- Tests/test_interactions.py
+++ Tests/test_interactions.py
@@ -8,11 +8,28 @@ class TestInteractions(unittest.TestCase):
 
         with self.assertRaises(LookupError):
             interactions.get_random_question_from_parameters(discipline="NONEXISTENT")
+        with self.assertRaises(LookupError):
+            interactions.get_random_question_from_parameters(discipline="MATEMÁTICA", area="NONEXISTENT")
 
+        with self.assertRaises(TypeError):
+            interactions.get_random_question_from_parameters(discipline=3)
         with self.assertRaises(TypeError):
             interactions.get_random_question_from_parameters(discipline=None)
 
         self.assertEqual(interactions.get_random_question_from_parameters(discipline="TESTING"), "testing")
+
+        with self.assertRaises(LookupError):
+            interactions.get_random_question_from_parameters(discipline="TESTING", difficulty_level=(0,2))
+        with self.assertRaises(LookupError):
+            interactions.get_random_question_from_parameters(discipline="TESTING", difficulty_level=(4,2))
+
+        self.assertEqual(
+            interactions.get_random_question_from_parameters(discipline="TESTING", difficulty_level=(0,3)), "testing")
+        self.assertEqual(
+            interactions.get_random_question_from_parameters(discipline="TESTING", difficulty_level=(0, 10)), "testing")
+
+        with self.assertRaises(TypeError):
+            interactions.get_random_question_from_parameters(discipline="TESTING", difficulty_level="one")
 
 
 if __name__ == '__main__':
