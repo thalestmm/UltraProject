6c86657ba5425beed0223bfbcb96b11069b7bcb5
thalestmm <meier.thales@gmail.com>
2022-07-25 14:59:31 -0300

    started implementing the navbar

--- UltraProject/marketing/urls.py
+++ UltraProject/marketing/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("optin/<str:lead_label>", views.lead_optin, name="optin"),
     path("obrigado/<str:lead_label>", views.thank_you, name="ty_page"),
     path("email-campaign", views.email_campaign, name="email_campaign"),
-    path("mail-unsubscribe/<str:user_uuid>", views.unsubscribe_from_mailing_list, name="unsubscribe")
+    path("unsubscribe/<str:user_uuid>", views.unsubscribe_from_mailing_list, name="unsubscribe")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
